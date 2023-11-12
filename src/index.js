b5fd183a65972000e45f71d71e2d17e288cbd9bf
Adriano <adriankox@macbook-pro-adriano.home>
2023-11-12 18:36:45 +0100

    siema

--- src/index.js
+++ src/index.js
@@ -1,17 +1,122 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+const data = [
+    {
+        name: "wow",
+        ingredients: "serio",
+        soldOut: false
+    },
+    {
+        name: "wowniesamowite",
+        ingredients: "serio to prawda",
+        soldOut: false
+    },
+    {
+        name: "wow to chuba magia",
+        ingredients: "serio nie wierze ",
+        soldOut: false
+    },
+    {
+        name: "wow mhm",
+        ingredients: "serio nie dasz wiary a tu serio",
+        soldOut: true
+    },
+]
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App () {
+    return (
+        <div className='container'>
+            <Header/>
+            <Menu/>
+            <Footer/>
+        </div>
+    )
+}
+
+function Header () {
+    return (
+        <header className='header'>
+            <h1>Fast React Pizza Co.</h1>
+        </header>
+    )
+}
+
+function Menu () {
+    const pizzas = data;
+    const dataLength = pizzas.length;
+    return (
+        <div className='menu'>
+            <h2>Our Menu</h2>
+
+            
+            {dataLength > 0 ? (
+                <>
+                    <p>
+                        the best italian pizza in town, maybe in the world! 
+                        buy now and enjoy new better life with our special ingredients
+                    </p>
+                    <ul className='pizzas'>
+                        {pizzas.map((pizza) => {
+                            return(
+                                <Pizza key={pizza.name} pizza={pizza}/>
+                                )
+                        })}
+                    </ul>
+                </>
+            ) : <p>sorry we dont have menu yet</p>}
+
+        </div>
+    )
+}
+
+function Footer () {
+    const hour = new Date().getHours();
+    const openHour = 12;
+    const closeHour = 23;
+    const isOpen = (hour >= openHour && hour <= closeHour) 
+
+
+    return (
+        <footer className='footer'>
+            {isOpen ? (
+                <Order closeHour={closeHour}/>
+            ) : <p>we are closed</p>}
+        </footer>
+    )
+}
+
+function Order ({closeHour}) {
+    return (
+        <div className='order'>
+                We are currently open until {closeHour}. Come and visit us or order online!
+                <button className='btn'>Order</button>
+        </div>
+    )
+}
+function Pizza ({pizza}) {
+    return (
+        <div className={`pizza ${pizza.soldOut ? 'sold-out' : ''}`}>
+            <img/>
+            <div>
+                <h3>
+                    {pizza.name}
+                </h3>
+                <p>
+                    {pizza.ingredients}
+                </p>
+                <span>
+                    {pizza.soldOut ? "Sold out" : '10'}
+                </span>
+            </div>
+        </div>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
